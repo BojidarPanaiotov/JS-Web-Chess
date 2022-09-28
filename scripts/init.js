6cdc5c009c00d2c594afcc353edbc039bc4d6795
Bozhidar Panayotov <cccpisthebest@abv.bg>
2022-09-28 11:27:56 +0300

    Function which is drawing the chest board

--- scripts/init.js
+++ scripts/init.js
@@ -1,1 +1,25 @@
+const chestBoard = document.querySelector('#chess-board');
 
+function drawChessBoard() {
+  for (let rowIndex = 0; rowIndex < 8; rowIndex++) {
+    let row = document.createElement('div');
+    row.setAttribute('class','row');
+  
+    for (let boxIndex = 0; boxIndex < 8; boxIndex++) {
+      let box = document.createElement('div');
+      box.setAttribute('id', rowIndex + ' ' + boxIndex);
+    
+      if((boxIndex+rowIndex) % 2 === 1) {
+        box.style.backgroundColor = '#E97451';
+      }
+  
+      box.setAttribute('class','box');
+  
+      row.appendChild(box);
+    }
+  
+    chestBoard.appendChild(row);
+  }
+}
+
+drawChessBoard();
