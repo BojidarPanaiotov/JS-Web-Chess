02dffb341862b86f698593c337b22f8e20f32e49
Bozhidar Panayotov <cccpisthebest@abv.bg>
2022-10-01 10:30:05 +0300

    Partial implementation of knight movement

--- scripts/figures/knight.js
+++ scripts/figures/knight.js
@@ -4,4 +4,18 @@ export default class Knight extends Figure {
     constructor(initialX, initialY,figureIcon,color) {
         super(initialX, initialY,figureIcon,color);
     }
+
+    getPossibleMoves(matrix) {
+        let whiteMoves = [{x: -2,y: +1},{x: -2,y: +1},{x: -2,y: -1},{x: -2,y: +1}];
+        let blackMoves = [{x: +2,y: 0},{x: +1,y: 0},{x: +1,y: +1},{x: +1,y: -1}];
+
+        let moves = this.color === 'white' ? whiteMoves : blackMoves;
+        let indexChecker = this.color === 'white' ? -1 : +1;
+        
+        for (let i = 1; i < 4; i++) {
+            moves.push({x: this.currentX + moves[i].x,y: this.currentY + moves[i].y})
+            
+        }
+        return this.calculateCoordinatesFromOrigin(matrix,moves);
+    }
 }
