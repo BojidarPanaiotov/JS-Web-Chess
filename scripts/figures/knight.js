4ccd312507163ad7e2855310067a7f7c126c66f9
Bozhidar Panayotov <cccpisthebest@abv.bg>
2022-10-01 13:42:07 +0300

    Knight movement in all directions implemented

--- scripts/figures/knight.js
+++ scripts/figures/knight.js
@@ -6,16 +6,20 @@ export default class Knight extends Figure {
     }
 
     getPossibleMoves(matrix) {
-        let whiteMoves = [{x: -2,y: +1},{x: -2,y: +1},{x: -2,y: -1},{x: -2,y: +1}];
-        let blackMoves = [{x: +2,y: 0},{x: +1,y: 0},{x: +1,y: +1},{x: +1,y: -1}];
-
-        let moves = this.color === 'white' ? whiteMoves : blackMoves;
-        let indexChecker = this.color === 'white' ? -1 : +1;
+        let moves = [
+            {x: -2,y: +1},{x: -2,y: +1},{x: -2,y: -1},{x: -2,y: +1},
+            {x: +2,y: -1},{x: +2,y: -1},{x: +2,y: +1},{x: +2,y: -1},
+            {x: -1,y: +2},{x: -1,y: +2},{x: -1,y: -2},{x: -1,y: +2},
+            {x: +1,y: -2},{x: +1,y: -2},{x: +1,y: +2},{x: +1,y: -2}
+        ];
         
-        for (let i = 1; i < 4; i++) {
-            moves.push({x: this.currentX + moves[i].x,y: this.currentY + moves[i].y})
+        let possibleMoves = [];
+
+        for (let i = 1; i < moves.length; i++) {
+            possibleMoves.push({x: this.currentX + moves[i].x,y: this.currentY + moves[i].y})
             
         }
-        return this.calculateCoordinatesFromOrigin(matrix,moves);
+
+        return this.calculateCoordinatesFromOrigin(matrix, possibleMoves);
     }
 }
