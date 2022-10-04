ea31be78e5bfd789b0c9dbe1e0a5f4c223ddb16e
Bozhidar Panayotov <cccpisthebest@abv.bg>
2022-10-04 17:00:36 +0300

    New background animation added

---
+++ scripts/utils/particlesConfiguration.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    Particles.init({
+        selector: '#background',
+        maxParticles: 250,
+        color: ['#DA0463', '#404B69', '#DBEDF3'],
+        sizeVariations: 10,
+        connectParticles: true,
+    });
+}
